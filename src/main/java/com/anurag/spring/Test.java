5e0ccdf4e81f56c53b0edadb84d444e6bf459515
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-15 15:01:29 +0530

    Inserted a record into DB using JDBCTemplate

--- src/main/java/com/anurag/spring/Test.java
+++ src/main/java/com/anurag/spring/Test.java
@@ -1,7 +1,20 @@
 package com.anurag.spring;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class Test {
     public static void main(String[] args) {
-        
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/anurag/spring/config.xml");
+
+        JdbcTemplate jdbcTemplate = (JdbcTemplate)ctx.getBean("jdbcTemplate");
+
+        String sql = "INSERT INTO employee(firstname, lastname) values(?, ?)";
+
+        int result = jdbcTemplate.update(sql, "Virat", "Kohli");
+
+        System.out.println("Number of records inserted: "+result);
+
+        ctx.close();
     }
 }
