2d4fa3592af49f8c09969e06e4f85173aafe25fd
Anurag Kulkarni <kanurag0801@gmail.com>
2022-04-17 14:09:18 +0530

    Created Friends Class

---
+++ src/main/java/com/anurag/spring/Friends/Friends.java
@@ -0,0 +1,36 @@
+package com.anurag.spring.Friends;
+
+public class Friends {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Friends [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + "]";
+    }
+}
